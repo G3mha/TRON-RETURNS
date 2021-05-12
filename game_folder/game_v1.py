8822f99a7e9417541387f2b6d15fc080a6ea979d
G3mha <riccogemha@gmail.com>
2021-05-12 20:57:26 -0300

    another one

--- game_folder/game_v1.py
+++ game_folder/game_v1.py
@@ -9,22 +9,21 @@ surface = pygame.display.set_mode((500,500))  # cria a tela do jogo com tamanho 
 file = os.path.join("sprites", "SPRITE SPIDER MAN - Copia.png")
 
 try:
-    image = pygame.image.load(file)
+    image = pygame.image.load("file")
 except pygame.error:
     print("Erro ao tentar ler imagem: SPRITE SPIDER MAN - Copia.png")
     sys.exit()
 
-surface.fill((0,0,0))  # preenche o display em preto
-
-surface.blit(image, (0,0))
-
-pygame.draw.circle(surface, (255,0,0), (250,250), 50)  # cria um círculo vermelho
-
-pygame.display.update()  # atualiza o que é mostrado na tela
+clock = pygame.time.Clock()
 
 while True:  # Loop infinito do game
+    clock.tick(60)  # segura a taxa de quadros em 60 por segundo
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
             pygame.quit()  # termina o pygame
             sys.exit()  # sai do jogo sem aparecer mensagem de erro
+    surface.fill((0,0,0))  # preenche o display em preto
+    surface.blit(image, (0,0))  # mostra a imagem na tela
+    pygame.draw.circle(surface, (255,0,0), (250,250), 50)  # cria um círculo vermelho
+    pygame.display.flip()  # atualiza o que é mostrado na tela
