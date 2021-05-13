9bfecd17de05c6bdfb478b409234814fba0c460a
G3mha <riccogemha@gmail.com>
2021-05-13 15:01:15 -0300

    final_aula2

--- game_folder/game_v1.py
+++ game_folder/game_v1.py
@@ -14,14 +14,17 @@ except pygame.error:
 
 clock = pygame.time.Clock()
 
+position = 0  # variável fria
+
 while True:  # Loop infinito do game
-    clock.tick(60)  # segura a taxa de quadros em 60 por segundo
+    time = clock.tick(60)  # segura a taxa de quadros em 60 por segundo
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
             pygame.quit()  # termina o pygame
             sys.exit()  # sai do jogo sem aparecer mensagem de erro
     surface.fill((0,0,0))  # preenche o display em preto
-    surface.blit(image, (0,0))  # mostra a imagem na tela
+    position += 0.2 * time # faz com que a imagem se mova a cada atualização
+    surface.blit(image, [position,0])  # mostra a imagem na tela
     pygame.draw.circle(surface, (255,0,0), (250,250), 50)  # cria um círculo vermelho
     pygame.display.flip()  # atualiza o que é mostrado na tela
