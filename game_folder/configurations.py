4ed87a3c0e6837b73618c627518d4220578b32b7
G3mha <riccogemha@gmail.com>
2021-05-21 10:15:51 -0300

    consertando o funcionamento das classes

---
+++ game_folder/configurations.py
@@ -0,0 +1,11 @@
+from os import path
+
+# Estabelece a pasta que contem as sprites.
+blue_dir = 'SPRITES/SPRITE_TRON_LIGHTCICLE_blue.png'
+yellow_dir = 'SPRITES/SPRITE_TRON_LIGHTCICLE_yellow.png'
+derezzed_dir = 'AUDIO/Derezzed.mp3'
+
+# Algumas variáveis essenciais para a aplicação
+screen_size = (1024,768) # Largura e altura da tela
+frames = 60
+lightcicle_size = (20,50) # Tamanho da bike
--- game_folder/grid_bike.py
+++ game_folder/grid_bike.py
@@ -7,18 +7,7 @@ Data: 18/05/2021
 import random
 import sys
 import pygame
-from os import path
-
-# Estabelece a pasta que contem as sprites.
-sprite_dir = path.join(path.dirname(__file__), 'SPRITE_TRON_LIGHTCICLE_blue.png')
-audio_dir = str(path.join(path.dirname(__file__), 'Derezzed.mp3'))
-audio_dir = audio_dir.replace("\Derezzed.mp3","/Derezzed.mp3")
-
-
-# Algumas variáveis essenciais para a aplicação
-screen_size = (1024,768) # Largura e altura da tela
-lightcicle_size = (20,50)
-page_title = "Corrida de motos"
+from configurations import derezzed_dir, blue_dir, yellow_dir, frames, screen_size, lightcicle_size
 
 # Define o código RGB das cores que utilizadas
 BLUE_MIDNIGHT = (0,0,30)
@@ -26,23 +15,36 @@ BLUE = (12,12,100)
 BLUE_LIGHT = (0,0,195)
 YELLOW_GOLD = (255,215,0)
 
+# Define o nome desta página
+page_title = "Corrida de motos"
+
 class yellowLightCicle():
-    def __init__(self):
-        self.image = pygame.image.load("SPRITE_TRON_LIGHTCICLE_yellow.png").convert_alpha()
+    def __init__(self, screen_size, yellow_dir, lightcicle_size):
+        self.image = pygame.image.load(yellow_dir).convert_alpha()
         pygame.transform.scale(self.image, lightcicle_size, None)
         self.rect = self.image.get_rect()
         self.set_position(screen_size[1], random.randint(0, screen_size[0]))
-        self.velocity = 0.4 # VALOR TESTE
+        self.set_velocity() # VALOR TESTE
+        self.direction = "DOWN"
+        self.last_direction
 
     def set_position(self, x, y):
         self.position = pygame.math.Vector2(x, y)
     
+    def set_velocity(self, vx, vy):
+        self.velocity = pygame.math.Vector2(vx, vy)
+    
     def update(self, time, direction):
+        self.direction = direction
+        if self.last_direction != self.direction:
+            # TODO muda a posicao da sprite
+            x = 
         self.position += self.velocity * time
+        self.last_direction = direction
 
 class blueLightCicle():
     def __init__(self):
-        self.image = pygame.image.load("SPRITE_TRON_LIGHTCICLE_blue.png").convert_alpha()
+        self.image = pygame.image.load(blue_dir).convert_alpha()
         pygame.transform.scale(self.image, lightcicle_size, None)
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
@@ -54,6 +56,9 @@ class blueLightCicle():
 
     def update(self, time, direction):
         self.position += self.velocity * time
+    
+    def direction(self):
+        e
 
 
 # Rotina Inicial do jogo
@@ -76,8 +81,8 @@ for static_lines in range(1,9):
     pygame.draw.line(surface, BLUE_MIDNIGHT, ((static_lines*static_vertical),0), ((static_lines*static_vertical),768), thickness) # linha vertical
 
 # Rotinas de aúdio
-pygame.mixer.music.load("AUDIO\Derezzed.mp3")
-pygame.mixer.music.set_volume(0.05)  # VALOR TESTE
+pygame.mixer.music.load(derezzed_dir)
+pygame.mixer.music.set_volume(0.04)  # VALOR TESTE
 pygame.mixer.music.play(-1)  # VALOR TESTE
 
 # Início do main Loop
