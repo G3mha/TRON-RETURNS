ac2d33977848c81c293b7c43b39c449fdacac586
G3mha <riccogemha@gmail.com>
2021-05-26 22:48:26 -0300

    aparecendo sprite pela primeira vez

--- game_folder/grid_bike.py
+++ game_folder/grid_bike.py
@@ -7,7 +7,24 @@ Data: 18/05/2021
 import random
 import sys
 import pygame
-from configurations import derezzedSONG_dir, derezzedSFX_dir, derezzedVFX_dir, frames, screen_size, lightcicle_size, LEFTblue_dir,RIGHTblue_dir,UPblue_dir,DOWNblue_dir,LEFTyellow_dir,RIGHTyellow_dir,UPyellow_dir,DOWNyellow_dir
+
+# Estabelece a pasta que contem as sprites.
+LEFTblue_dir = 'SPRITES/SPRITE_TRON_LIGHTCICLE_blueLEFT.png'
+RIGHTblue_dir = 'SPRITES/SPRITE_TRON_LIGHTCICLE_blueRIGHT.png'
+UPblue_dir = 'SPRITES/SPRITE_TRON_LIGHTCICLE_blueUP.png'
+DOWNblue_dir = 'SPRITES/SPRITE_TRON_LIGHTCICLE_blueDOWN.png'
+LEFTyellow_dir = 'SPRITES/SPRITE_TRON_LIGHTCICLE_yellowLEFT.png'
+RIGHTyellow_dir = 'SPRITES/SPRITE_TRON_LIGHTCICLE_yellowRIGHT.png'
+UPyellow_dir = 'SPRITES/SPRITE_TRON_LIGHTCICLE_yellowUP.png'
+DOWNyellow_dir = 'SPRITES/SPRITE_TRON_LIGHTCICLE_yellowDOWN.png'
+derezzedSONG_dir = 'AUDIO/DerezzedSong.ogg'
+derezzedSFX_dir = 'AUDIO/DerezzedFX.ogg'
+derezzedVFX_dir = 'SPRITES/VFX DEREZZED EXPLOSION.png'
+
+# Algumas variáveis essenciais para a aplicação
+screen_size = (1024,768) # Largura e altura da tela
+lightcicle_size = (200,100) # Tamanho da bike
+page_title = "Corrida de motos" # Define o nome desta página
 
 # Define o código RGB das cores que utilizadas
 BLUE_MIDNIGHT = (0,0,30)
@@ -15,16 +32,14 @@ BLUE = (12,12,100)
 BLUE_LIGHT = (0,0,195)
 YELLOW_GOLD = (255,215,0)
 
-# Define o nome desta página
-page_title = "Corrida de motos"
 
 class yellowLightCicle(pygame.sprite.Sprite):
-    def __init__(self, group, screen_size, LEFTyellow_dir, lightcicle_size):
+    def __init__(self, group, LEFTyellow_dir):
         super().__init__(group)
         self.image = pygame.image.load(LEFTyellow_dir).convert_alpha()
-        pygame.transform.scale(self.image, lightcicle_size)
+        self.image = pygame.transform.scale(self.image, (int(self.image.get_width()/5),int(self.image.get_height()/5)))
         self.rect = self.image.get_rect()
-        self.set_position(0,0) # screen_size[0], random.randint(0, screen_size[1])
+        self.set_position(1024,0)
         self.set_velocity(-0.4,0) # VALOR TESTE
         self.direction = "LEFT"
         self.trace = []
@@ -35,64 +50,29 @@ class yellowLightCicle(pygame.sprite.Sprite):
     def set_velocity(self, vx, vy):
         self.velocity = pygame.math.Vector2(vx, vy)
     
-    def update_direction(self, direction):
+    def update_direction(self, direction,LEFTyellow_dir,RIGHTyellow_dir,UPyellow_dir,DOWNyellow_dir):
         if direction != self.direction:
             if direction == "UP":
                 self.image = pygame.image.load(UPyellow_dir).convert_alpha()
+                self.image = pygame.transform.scale(self.image, (int(self.image.get_width()/5),int(self.image.get_height()/5)))
                 self.set_velocity(0,0.4) # VALOR TESTE
             if direction == "DOWN":
                 self.image = pygame.image.load(DOWNyellow_dir).convert_alpha()
+                self.image = pygame.transform.scale(self.image, (int(self.image.get_width()/5),int(self.image.get_height()/5)))
                 self.set_velocity(0,-0.4) # VALOR TESTE
             if direction == "LEFT":
                 self.image = pygame.image.load(LEFTyellow_dir).convert_alpha()
+                self.image = pygame.transform.scale(self.image, (int(self.image.get_width()/5),int(self.image.get_height()/5)))
                 self.set_velocity(-0.4,0) # VALOR TESTE
             if direction == "RIGHT":
                 self.image = pygame.image.load(RIGHTyellow_dir).convert_alpha()
+                self.image = pygame.transform.scale(self.image, (int(self.image.get_width()/5),int(self.image.get_height()/5)))
                 self.set_velocity(0.4,0) # VALOR TESTE
             self.direction = direction
 
     def update_position(self, time):
-        self.last_position = self.position
         self.position += self.velocity * time
-        # if self.position != self.last_position:
-        #     self.trace.append(self.position)
-
-class blueLightCicle():
-    def __init__(self, screen_size, RIGHTblue_dir, lightcicle_size):
-        self.image = pygame.image.load(RIGHTblue_dir).convert_alpha()
-        pygame.transform.scale(self.image, lightcicle_size, None)
-        self.rect = self.image.get_rect()
-        self.set_position(screen_size[0], random.randint(0, screen_size[1]))
-        self.set_velocity(0.4,0) # VALOR TESTE
-        self.direction = "RIGHT"
-
-    def set_position(self, x, y):
-        self.position = pygame.math.Vector2(x, y)
-    
-    def set_velocity(self, vx, vy):
-        self.velocity = pygame.math.Vector2(vx, vy)
-    
-    def update_direction(self, direction):
-        if direction != self.direction:
-            if direction == "UP":
-                self.image = pygame.image.load(UPblue_dir).convert_alpha()
-                self.set_velocity(0,0.4) # VALOR TESTE
-            if direction == "DOWN":
-                self.image = pygame.image.load(DOWNblue_dir).convert_alpha()
-                self.set_velocity(0,-0.4) # VALOR TESTE
-            if direction == "LEFT":
-                self.image = pygame.image.load(LEFTblue_dir).convert_alpha()
-                self.set_velocity(-0.4,0) # VALOR TESTE
-            if direction == "RIGHT":
-                self.image = pygame.image.load(RIGHTblue_dir).convert_alpha()
-                self.set_velocity(0.4,0) # VALOR TESTE
-            self.direction = direction
-
-    def update_position(self, time):
-        self.last_position = self.position
-        self.position += self.velocity * time
-        if self.position != self.last_position:
-            self.trace.append(self.position)
+        # self.trace.append(self.position)
 
 
 # Rotina Inicial do jogo
@@ -109,6 +89,10 @@ pygame.mixer.music.play(-1)  # VALOR TESTE
 # variável que declara o clock do jogo
 clock = pygame.time.Clock()
 
+# cria sprite da Moto Amarela
+sprites = pygame.sprite.Group()
+yellow = yellowLightCicle(sprites, LEFTyellow_dir)
+
 # Início do main Loop
 while True:
     time = clock.tick(60) # segura a taxa de quadros em 60 por segundo
@@ -120,17 +104,17 @@ while True:
                 sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                yellow.update_direction("LEFT")
+                yellow.update_direction("LEFT",LEFTyellow_dir,RIGHTyellow_dir,UPyellow_dir,DOWNyellow_dir)
             elif event.key == pygame.K_RIGHT:
-                yellow.update_direction("RIGHT")
+                yellow.update_direction("RIGHT",LEFTyellow_dir,RIGHTyellow_dir,UPyellow_dir,DOWNyellow_dir)
             elif event.key == pygame.K_UP:
-                yellow.update_direction("UP")
+                yellow.update_direction("UP",LEFTyellow_dir,RIGHTyellow_dir,UPyellow_dir,DOWNyellow_dir)
             elif event.key == pygame.K_DOWN:
-                yellow.update_direction("DOWN")
-    
+                yellow.update_direction("DOWN",LEFTyellow_dir,RIGHTyellow_dir,UPyellow_dir,DOWNyellow_dir)
+
     # Rotinas da tela "estática"
     surface.fill(BLUE)
-    thickness = 15  # VALOR TESTE
+    thickness = 10
     # Desenha borda da tela
     pygame.draw.line(surface, BLUE_MIDNIGHT, (0,0), (screen_size[0],0), thickness)
     pygame.draw.line(surface, BLUE_MIDNIGHT, (0,0), (0,screen_size[1]), thickness)
@@ -142,10 +126,8 @@ while True:
         pygame.draw.line(surface, BLUE_MIDNIGHT, (0,(static_lines*static_horizontal)), (1024,(static_lines*static_horizontal)), thickness) # linha horizontal
         pygame.draw.line(surface, BLUE_MIDNIGHT, ((static_lines*static_vertical),0), ((static_lines*static_vertical),768), thickness) # linha vertical
 
-    # cria sprite da Moto Amarela
-    sprites = pygame.sprite.Group()
-    yellow = yellowLightCicle(screen_size, LEFTyellow_dir, lightcicle_size)
-    yellow.render()
+    sprites.draw(surface)
+    yellow.update_position(time)
 
 
     # if trace.rect.collidepoint( ): # TODO definir trace
@@ -154,9 +136,5 @@ while True:
     #         sprite.kill()
     #         score += 1
 
-    yellow.update_position(time)
-
 
     pygame.display.update() # atualiza o display
-
-pygame.quit() # encerra o pygame
