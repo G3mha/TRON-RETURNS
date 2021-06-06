4f4eb80e588ec1dc17b52ac85f1f8eca4506f5d4
Brunobesno <brunowb1@al.insper.edu.br>
2021-06-06 17:52:02 -0300

    classe disk

--- game_folder/_SPIDERMAN GAME TEST/disc_wars.py
+++ game_folder/_SPIDERMAN GAME TEST/disc_wars.py
@@ -3,19 +3,10 @@ import sys
 import math
 
 class Disk(object):
-    def __init__(self, player_rect):
-        self.rect = ('x','y','width','height') # TODO: change values
+    def __init__(self):
+        self.image = pygame.image.load('SPRITES_BOSS/neutral_disk.png').convert_alpha()
+        self.rect = ('x','y',100,100) # TODO: change values for x & y
         self.velocity = (0,0) # TODO: change values
-        self.images = []
-        self.images.append(pygame.image.load('SPRITES_BOSS/disk_orange.png').convert_alpha())
-        self.images.append(pygame.image.load('SPRITES_BOSS/disk_blue.png').convert_alpha())
-
-    def define_color(self, colour):
-        if colour == "orange":
-            self.image = self.images[0]
-        if colour == "blue":
-            self.image = self.images[1]
 
     def update(self, time):
         self.rect.center += self.velocity * time
-        
