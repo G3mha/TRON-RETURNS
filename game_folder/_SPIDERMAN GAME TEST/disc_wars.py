71e1f81a1eccc559d1f483b920544adf0cfc1854
Brunobesno <brunowb1@al.insper.edu.br>
2021-06-06 18:19:06 -0300

    Adição da classe paddle

--- game_folder/_SPIDERMAN GAME TEST/disc_wars.py
+++ game_folder/_SPIDERMAN GAME TEST/disc_wars.py
@@ -2,6 +2,9 @@ import pygame
 import sys
 import math
 
+BLUE_ICE = (0,255,251)
+YELLOW_GOLD = (255,215,0)
+
 class Disk(object):
     def __init__(self):
         self.image = pygame.image.load('SPRITES_BOSS/neutral_disk.png').convert_alpha()
@@ -10,3 +13,22 @@ class Disk(object):
 
     def update(self, time):
         self.rect.center += self.velocity * time
+
+class Paddle(object):
+    def __init__(self, colour, surface):
+        if colour == "blue":
+            pygame.draw.rect(surface, BLUE_ICE, (0,0,0,0)) # TODO: change values
+        if colour == "yellow":
+            pygame.draw.rect(surface, YELLOW_GOLD, (0,0,0,0)) # TODO: change values
+        self.rect = ('x','y',100,100) # TODO: change values for x & y
+        self.velocity = (0,0)
+
+    def game_controls(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                self.velocity = (0,0) # TODO: change values
+            elif event.key == pygame.K_UP:
+                self.velocity = (0,0) # TODO: change values
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN or event.key == pygame.K_UP:
+                self.velocity = (0,0)
