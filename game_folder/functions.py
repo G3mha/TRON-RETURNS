e776b87708f27d69278dc295ad0e8991facc862d
Maraba23 <rabello.barranco@gmail.com>
2021-06-09 23:50:35 -0300

    documantacao 2

--- game_folder/functions.py
+++ game_folder/functions.py
@@ -348,6 +348,13 @@ class Disk_BF(pygame.sprite.Sprite):
         self.rect = pygame.Rect(rect[0], rect[1], 31, 28)
     
     def set_position(self, x, y):
+
+        """
+        Parameters
+        ----------
+        midbottom:
+        """
+
         self.rect.midbottom = pygame.math.Vector2(x, y)
     
     def set_velocity(self, vx, vy):
